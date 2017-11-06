1e5ddee82df493674b64555f9edb2c56ec6c3385
inshad2005 <promatics.inshad@gmail.com>
2017-11-06 11:52:33 +0530

    dsfedgdegdgdgd

---
+++ src/app/models/stateResources.ts
@@ -0,0 +1,92 @@
+export class StringResource {
+
+		language=[
+				{'language':"Hindi"},
+				{'language':"Marathi"},
+				{'language':"Assamese" },
+				{'language':"Bengali" },
+				{'language':"Dogri" },
+				{'language':"Gujarati" },
+				{'language':" Punjabi" },
+				{'language':"Bodo" },
+				{'language':"Kannada" },
+				{'language':"Kashmiri" },
+				{'language':"Urdu" },
+				{'language':"Telegu" },
+				{'language':"Konkani" },
+				{'language':"Malayalam" },
+				{'language':"Manipuri or Meithei" },
+				{'language':"English" },
+				{'language':"Nepali" },
+				{'language':"Oriya" },
+				{'language':"Sanskrit" },
+				{'language':"Santali" },
+				{'language':"Sindhi" },
+				{'language':"Tamil"}
+		      ]
+	      stateList= [
+				 {stateName: "Andhra Pradesh"},
+				 {stateName: "Arunachal Pradesh"},
+				 {stateName: "Assam"},
+				 {stateName: "Bihar"},
+				 {stateName: "Chhattisgarh"},
+				 {stateName: "Goa"},
+				 {stateName: "Gujarat"},
+				 {stateName: "Haryana"},
+				 {stateName: "Himachal Pradesh"},
+				 {stateName: "Jammu and Kashmir"},
+				 {stateName: "Jharkhand"},
+				 {stateName: "Karnataka"},
+				 {stateName: "Kerala"},
+				 {stateName: "Madhya Pradesh"},
+				 {stateName: "Maharashtra"},
+				 {stateName: "Manipur"},
+				 {stateName: "Meghalaya"},
+				 {stateName: "Mizoram"},
+				 {stateName: "Nagaland"},
+				 {stateName: "Odisha"},
+				 {stateName: "Punjab"},
+				 {stateName: "Rajasthan"},
+				 {stateName: "Sikkim"},
+				 {stateName: "Tamil Nadu"},
+				 {stateName: "Telangana"},
+				 {stateName: "Tripura"},
+				 {stateName: "Uttar Pradesh"},
+				 {stateName: "Uttarakhand"},
+				 {stateName: "West Bengal"},
+				 {stateName: "Andaman and Nicobar Islands"},
+				 {stateName: "Chandigarh"},
+				 {stateName: "Dadra and Nagar Haveli"},
+				 {stateName: "Daman and Diu"},
+				 {stateName: "National Capital Territory of Delhi"},
+				 {stateName: "Lakshadweep"},
+				 {stateName: "Pondicherry "}
+				];
+
+	     categoryTemplate=[
+	           {templateName:"Category-view Template One"	, "template":"category-view1.png" ,status:"inactive"},
+	           {templateName:"Category-view Template Two"	, "template":"category-view2.png" ,status:"inactive"},
+	           {templateName:"Category-view Template Three", "template":"category-view3.png" ,status:"inactive"},
+	           {templateName:"Category-view Template Four", "template":"category-view4.png" ,status:"inactive"},
+
+	     ]
+
+	     listingTemplate=[
+	           {templateName:"Listing-view Template One"	, "template":"listing-view1.png" , status:"inactive"},
+	           {templateName:"Listing-view Template Two"	, "template":"listing-view2.png" , status:"inactive"},
+	           {templateName:"Listing-view Template Three", "template":"listing-view3.png" , status:"inactive"},
+	           {templateName:"Listing-view Template Five"	, "template":"listing-view5.png" , status:"inactive"},
+	           {templateName:"Listing-view Template Six"	, "template":"listing-view6.png" , status:"inactive"},
+	           {templateName:"Listing-view Template Seven", "template":"listing-view7.png" , status:"inactive"},
+	           {templateName:"Listing-view Template Eight", "template":"listing-view8.png" , status:"inactive"},
+
+	     ]
+
+	    subCategoryTemplate=[
+	           {templateName:"SubCategory-view Template One"	, "template":"category-view1.png" ,status:"inactive"},
+	           {templateName:"SubCategory-view Template Two"	, "template":"category-view2.png" ,status:"inactive"},
+	           {templateName:"SubCategory-view Template Three", "template":"category-view3.png" ,status:"inactive"},
+	           {templateName:"SubCategory-view Template Four", "template":"category-view4.png" ,status:"inactive"},
+
+	     ]
+}
