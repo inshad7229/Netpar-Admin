7b552b3400391c81f61120952767c5e47d87105a
inshad2005 <promatics.inshad@gmail.com>
2017-12-13 19:36:36 +0530

    ssss

--- src/app/dashboard/element-analytics/element-analytics.component.ts
+++ src/app/dashboard/element-analytics/element-analytics.component.ts
@@ -118,15 +118,18 @@ export class ElementAnalyticsComponent implements OnInit {
     dataAfterSubCategory=[]
     contentDataAfterFilter
     filterStateFilterPan
-   filterState=[]
-   dataAfterState=[]
-   dataForSorting
+    filterState=[]
+    dataAfterState=[]
+    dataForSorting
+    arrangedData=[]
+    limitedFilter 
   constructor(private sectionService:SectionService,
             private appProvider: AppProvider,
             private adminService:AdminService,
             private contentService:ContentService) {
             this.filterValue={}
                 this.filterRequest={}
+                this.limitedFilter={}
                 this.sendData={} 
 
   }
@@ -159,10 +162,10 @@ export class ElementAnalyticsComponent implements OnInit {
                     if (results) {
                       this.contentData=results[0].response;
                       this.contentBackup=results[0].response;
-                      this.dataForSorting=results[0].response;
                       this.sectionData=results[1]
                       this.sectionsBack=results[1]
                       this.sections=this.sections.concat(this.sectionsBack)
+                      this.aggrigateDataMake()
                     }
                     
                     
@@ -172,6 +175,135 @@ export class ElementAnalyticsComponent implements OnInit {
                     this.waitLoader = false;
                 })
     }
+    aggrigateDataMake(){
+                     this.arrangedData=[]
+                     this.arrangedData.push({
+                                              tag:'Text Only',
+                                              pageView:this.getPageView('Text Only'),
+                                              uniqueViews:this.getUniquePageView('Text Only'),
+                                              likeCount:this.getLike('Text Only'),
+                                              shareCount:this.getShare('Text Only'),
+                                              commentCount:this.getComment('Text Only'),
+                                              saveCount:this.getSave('Text Only'),
+                                              downloadCount:this.getDownload('Text Only'),
+                                              applyCount:this.getApply('Text Only'),
+                                              callCount:this.getCall('Text Only'),
+                                              callMeBackCount:this.getCallMeBAck('Text Only'),
+                                              imIntrestedCount:this.getIntrested('Text Only')
+                                              })
+                      this.arrangedData.push({
+                                              tag:'Photo Only',
+                                              pageView:this.getPageView('Photo Only'),
+                                              uniqueViews:this.getUniquePageView('Photo Only'),
+                                              likeCount:this.getLike('Photo Only'),
+                                              shareCount:this.getShare('Photo Only'),
+                                              commentCount:this.getComment('Photo Only'),
+                                              saveCount:this.getSave('Photo Only'),
+                                              downloadCount:this.getDownload('Photo Only'),
+                                              applyCount:this.getApply('Photo Only'),
+                                              callCount:this.getCall('Photo Only'),
+                                              callMeBackCount:this.getCallMeBAck('Photo Only'),
+                                              imIntrestedCount:this.getIntrested('Photo Only')
+                                              })
+                     this.arrangedData.push({
+                                              tag:'Audio Only',
+                                              pageView:this.getPageView('Audio Only'),
+                                              uniqueViews:this.getUniquePageView('Audio Only'),
+                                              likeCount:this.getLike('Audio Only'),
+                                              shareCount:this.getShare('Audio Only'),
+                                              commentCount:this.getComment('Audio Only'),
+                                              saveCount:this.getSave('Audio Only'),
+                                              downloadCount:this.getDownload('Audio Only'),
+                                              applyCount:this.getApply('Audio Only'),
+                                              callCount:this.getCall('Audio Only'),
+                                              callMeBackCount:this.getCallMeBAck('Audio Only'),
+                                              imIntrestedCount:this.getIntrested('Audio Only')
+                                              })
+                     this.arrangedData.push({
+                                              tag:'Video Only',
+                                              pageView:this.getPageView('Video Only'),
+                                              uniqueViews:this.getUniquePageView('Video Only'),
+                                              likeCount:this.getLike('Video Only'),
+                                              shareCount:this.getShare('Video Only'),
+                                              commentCount:this.getComment('Video Only'),
+                                              saveCount:this.getSave('Video Only'),
+                                              downloadCount:this.getDownload('Video Only'),
+                                              applyCount:this.getApply('Video Only'),
+                                              callCount:this.getCall('Video Only'),
+                                              callMeBackCount:this.getCallMeBAck('Video Only'),
+                                              imIntrestedCount:this.getIntrested('Video Only')
+                                              })
+                      this.arrangedData.push({
+                                              tag:'Text + Video',
+                                              pageView:this.getPageView('Text + Video'),
+                                              uniqueViews:this.getUniquePageView('Text + Video'),
+                                              likeCount:this.getLike('Text + Video'),
+                                              shareCount:this.getShare('Text + Video'),
+                                              commentCount:this.getComment('Text + Video'),
+                                              saveCount:this.getSave('Text + Video'),
+                                              downloadCount:this.getDownload('Text + Video'),
+                                              applyCount:this.getApply('Text + Video'),
+                                              callCount:this.getCall('Text + Video'),
+                                              callMeBackCount:this.getCallMeBAck('Text + Video'),
+                                              imIntrestedCount:this.getIntrested('Text + Video')
+                                              })
+                       this.arrangedData.push({
+                                              tag:'Text + Audio',
+                                              pageView:this.getPageView('Text + Audio'),
+                                              uniqueViews:this.getUniquePageView('Text + Audio'),
+                                              likeCount:this.getLike('Text + Audio'),
+                                              shareCount:this.getShare('Text + Audio'),
+                                              commentCount:this.getComment('Text + Audio'),
+                                              saveCount:this.getSave('Text + Audio'),
+                                              downloadCount:this.getDownload('Text + Audio'),
+                                              applyCount:this.getApply('Text + Audio'),
+                                              callCount:this.getCall('Text + Audio'),
+                                              callMeBackCount:this.getCallMeBAck('Text + Audio'),
+                                              imIntrestedCount:this.getIntrested('Text + Audio')
+                                              })
+                       this.arrangedData.push({
+                                              tag:'Text + Photo',
+                                              pageView:this.getPageView('Text + Photo'),
+                                              uniqueViews:this.getUniquePageView('Text + Photo'),
+                                              likeCount:this.getLike('Text + Photo'),
+                                              shareCount:this.getShare('Text + Photo'),
+                                              commentCount:this.getComment('Text + Photo'),
+                                              saveCount:this.getSave('Text + Photo'),
+                                              downloadCount:this.getDownload('Text + Photo'),
+                                              applyCount:this.getApply('Text + Photo'),
+                                              callCount:this.getCall('Text + Photo'),
+                                              callMeBackCount:this.getCallMeBAck('Text + Photo'),
+                                              imIntrestedCount:this.getIntrested('Text + Photo')
+                                              })
+                       this.arrangedData.push({
+                                              tag:'Text + Multiple',
+                                              pageView:this.getPageView('Text + Multiple'),
+                                              uniqueViews:this.getUniquePageView('Text + Multiple'),
+                                              likeCount:this.getLike('Text + Multiple'),
+                                              shareCount:this.getShare('Text + Multiple'),
+                                              commentCount:this.getComment('Text + Multiple'),
+                                              saveCount:this.getSave('Text + Multiple'),
+                                              downloadCount:this.getDownload('Text + Multiple'),
+                                              applyCount:this.getApply('Text + Multiple'),
+                                              callCount:this.getCall('Text + Multiple'),
+                                              callMeBackCount:this.getCallMeBAck('Text + Multiple'),
+                                              imIntrestedCount:this.getIntrested('Text + Multiple')
+                                              })
+
+                     this.dataForSorting=this.arrangedData.slice();
+    }
+          // <td> {{getPageView('Text Only')}} </td>
+          //                                       <td> {{getUniquePageView('Text Only')}} </td>
+          //                                       <td> {{getLike('Text Only')}} </td>
+          //                                       <td> {{getShare('Text Only')}} </td>
+          //                                       <td> {{getComment('Text Only')}} </td>
+          //                                       <td> {{getSave('Text Only')}} </td>
+          //                                       <td> {{getDownload()}} </td>
+          //                                       <td> {{getApply('Text Only')}} </td>
+          //                                       <td> {{getCall('Text Only')}} </td>
+          //                                       <td> {{getCallMeBAck('Text Only')}} </td>
+          //                                       <td> {{getIntrested('Text Only')}} </td>
+
     getPageView(mimeType){
       let value=0
       let data= this.contentData.filter(arg=>arg.mimeType==mimeType)
@@ -467,7 +599,8 @@ onSelectstate(state){
        let finalData= this.dataAfterLanguage.concat(this.dataAfterSection,this.dataAfterCategory,this.dataAfterSubCategory);
        this.contentData=this.unique(finalData)
        this.contentDataAfterFilter=this.unique(finalData)
-       this.dataForSorting=this.unique(finalData)
+       this.aggrigateDataMake()
+      // this.dataForSorting=this.unique(finalData)
        this.filterLanguageFilterPan=this.filterLanguage.slice(0);
        this.filterSectionFilterPan=this.filterSection.slice(0);
        this.filterCategoryFilterPan=this.filterCategory.slice(0);
@@ -484,24 +617,29 @@ unique(array){
         onClearLangFilter(lang){
           this.contentData=this.contentData.filter(arg=>arg.language!=lang)
           this.filterLanguageFilterPan=this.filterLanguageFilterPan.filter(arg=>arg!=lang)
+           this.aggrigateDataMake()
         }
         onClearSectionFilter(secId){
             this.contentData=this.contentData.filter(arg=>arg.sectionId!=secId) 
-            this.filterSectionFilterPan=this.filterSectionFilterPan.filter(arg=>arg!=secId)         
+            this.filterSectionFilterPan=this.filterSectionFilterPan.filter(arg=>arg!=secId) 
+             this.aggrigateDataMake()        
         }
         onClearCategoryFilter(catId){
           console.log(JSON.stringify(this.filterCategoryFilterPan))
             this.contentData=this.contentData.filter(arg=>arg.categoryId!=catId)
             this.filterCategoryFilterPan=this.filterCategoryFilterPan.filter(arg=>arg._id!=catId)
+             this.aggrigateDataMake()
         }
         onClearSubCategoryFilter(subCatId){
             this.contentData=this.contentData.filter(arg=>arg.subCategorId!=subCatId)
             this.filterSubcategoryFilterPan=this.filterSubcategoryFilterPan.filter(arg=>arg._id!=subCatId)
+            this.aggrigateDataMake()
         }
 
         onClearStateFilter(state){
           this.contentData=this.contentData.filter(arg=>arg.applicableStateLists.indexOf(state.stateName)==-1)
           this.filterStateFilterPan=this.filterStateFilterPan.filter(arg=>arg!=state)
+          this.aggrigateDataMake()
         }
         clearAll(){
             this.filterLanguageFilterPan=[]
@@ -534,19 +672,20 @@ unique(array){
             for (let i=0;i<this.subCategory.length;i++) {
                this.subCategory[i].check=false
             }
+            this.aggrigateDataMake()
         }
 
-        sortData(sort: Sort) {
+   sortData(sort: Sort) {
     //  this.contentBackup
     // this.contentList
     const data =this.dataForSorting.slice();
     if (!sort.active || sort.direction == '') {
-      this.contentData = data;
+      this.arrangedData = data;
       
       return;
     }
 
-    this.contentData = data.sort((a, b) => {
+    this.arrangedData = data.sort((a, b) => {
       let isAsc = sort.direction == 'asc';
       switch (sort.active) {
         case 'Kadak': return compare(a.likeCount, b.likeCount, isAsc);
@@ -565,6 +704,38 @@ unique(array){
       }
     });
   }
+
+   onSortMulti(value){
+      //alert(value)
+    const data =this.dataForSorting.slice();
+    if (!value || value == '') {
+      this.arrangedData = data;
+      
+      return;
+    }
+
+    this.arrangedData.sort((a, b) => {
+      //alert(value)
+      let isAsc =  'asc';
+      switch (value) {
+
+        case 'Kadak': return compare2(a.likeCount, b.likeCount, isAsc);
+        case 'share': return compare2(a.shareCount, b.shareCount, isAsc);
+        case 'comment': return compare2(a.commentCount, b.commentCount, isAsc);
+        case 'save': return compare2(a.saveCount, b.saveCount, isAsc);
+        case 'download': return compare2(a.downloadCount, b.downloadCount, isAsc);
+        case 'apply': return compare2(a.applyCount, b.applyCount, isAsc);
+        case 'call': return compare2(a.callCount, b.callCount, isAsc);
+        case 'call_Me_Back': return compare2(a.callMeBackCount, b.callMeBackCount, isAsc);
+        case 'interested': return compare2(a.imIntrestedCount, b.imIntrestedCount, isAsc);
+        case 'pageViews': return compare2(a.pageView, b.pageView, isAsc);
+        case 'uniquePage': return compare2(a.uniqueViews, b.uniqueViews, isAsc);
+        case 'continue': return compare2(a.continueReading, b.continueReading, isAsc);
+        default: return 0;
+      }
+    });
+  }
+
 }
 
 function compare(a, b, isAsc) {
